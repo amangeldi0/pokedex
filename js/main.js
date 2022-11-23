9ce6619a691e1af38063e6e2de8ccaaf2c8c6971
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-23 13:35:44 +0600

    show hide function for search block

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 const searchButton = document.querySelector('#header__search__button')
 const searchInputBlock = document.querySelector('#header__search__input__block')
+
+searchButton.addEventListener('click', (event) => {
+    searchInputBlock.classList.toggle('active')
+    searchButton.classList.toggle('active')
+    window.addEventListener('click' , event => {
+        if (!event.target.closest('#header__search__button') && !event.target.closest('#header__search__input__block')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+            searchInputBlock.classList.remove('active')
+            searchButton.classList.remove('active')
+        }
+    })
+})
