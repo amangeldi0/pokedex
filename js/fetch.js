f406b8ddec7e03614c5979229b5ecad2ba39eb41
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-25 19:02:13 +0600

    fetch pokemon with properties

--- js/fetch.js
+++ js/fetch.js
@@ -1,2 +1,13 @@
 const pokemonContainer = document.querySelector('#pokemon__container')
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon/'
+
+
+async function fetchToPokemon(url, offset, limit) {
+    try{
+        const res = await fetch(`${url}?offset=${offset}&limit=${limit}`).then(res => res.json())
+        res.results.forEach(pokemon => detail(pokemon.url))
+
+    }catch (e) {
+        throw new Error(e)
+    }
+}
