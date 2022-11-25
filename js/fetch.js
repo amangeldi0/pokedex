d8b1f20e533746e552bb1eee88f883e910d6fc50
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-25 19:03:18 +0600

    detail show function which fetch and get pokemon details info

--- js/fetch.js
+++ js/fetch.js
@@ -11,3 +11,26 @@ async function fetchToPokemon(url, offset, limit) {
         throw new Error(e)
     }
 }
+const detail = async (url) => {
+    try {
+        const res = await fetch(url).then(response => response.json())
+            .then(pokemon => {
+                let div = document.createElement('div')
+                div.className = "pokemon__block";
+
+                div.innerHTML = `
+                <div class="pokemon__image">
+                    <img src=${pokemon.sprites['front_default']} alt="pokemon">
+                </div>
+                <div class="pokemon__name">${pokemon.name}</div>
+                <div class='pokemon__types ${pokemon.types[0].type.name}'>${pokemon.types[0].type.name}</div>
+                <button class="pokemon__add__favorite">
+                    <img src="./assets/favourite__icon.png" alt="favourite__icon">
+                </button>`;
+                pokemonContainer.append(div)
+
+            })
+    } catch (e) {
+        throw new Error(e)
+    }
+}
