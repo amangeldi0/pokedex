ef37d3e5f34be33890a1c5a8f91736f4ac636506
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-30 18:13:29 +0600

    rewrite pagination logic

--- js/pagination.js
+++ js/pagination.js
@@ -1,27 +1,62 @@
 
-const paginationButtons = document.querySelector('#pagination__buttons')
-const pokemonCount = JSON.parse(localStorage.getItem('allPokemons')).length
-let currentPage = 1
-let paginationLimit = 20
-const pageCount = Math.ceil(pokemonCount / paginationLimit);
-
-
-const appendPageNumber = (index) => {
-    const pageNumber = document.createElement("button");
-    pageNumber.className = "pagination-number";
-    pageNumber.innerHTML = index;
-    pageNumber.setAttribute("page-index", index);
-    paginationButtons.appendChild(pageNumber);
-};
-
-const getPaginationNumbers = (pokemon, pgLimit) => {
-    const result = Math.ceil(pokemon / pgLimit);
-    for (let i = 1; i <= result; i++) {
-        appendPageNumber(i);
+//pagination input lister
+paginationInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter'){
+        if (pageCount > Number(event.target.value)){
+            currentPage = Number(event.target.value)
+            toRenderPokemon(pokemons,paginationLimit * (currentPage - 1), paginationLimit * currentPage)
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            })
+            paginationInput.blur()
+        }else if(pageCount === Number(event.target.value)){
+            next.disabled = true
+            prev.disabled = false
+        }else{
+            paginationInput.value = pageCount
+            currentPage = pageCount
+            toRenderPokemon(pokemons,paginationLimit * (currentPage - 1), paginationLimit * currentPage)
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            })
+            paginationInput.blur()
+        }
     }
-};
+})
 
+// prev button listener
+prev.addEventListener('click', () => {
+    next.disabled = false
+    if (currentPage === 1){
+        prev.disabled = true
+    }
+    else{
+        prev.disabled = false
+        currentPage -= 1
+        paginationInput.value = currentPage
+        toRenderPokemon(pokemons,paginationLimit * (currentPage - 1), paginationLimit * currentPage)
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+})
 
-
-getPaginationNumbers(pokemonCount, paginationLimit)
+// next button listener
+next.addEventListener('click', () => {
+    prev.disabled = false
+    if (currentPage < pageCount){
+        currentPage += 1
+        paginationInput.value = currentPage
+        toRenderPokemon(pokemons,paginationLimit * (currentPage - 1), paginationLimit * currentPage)
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }else{
+        next.disabled = true
+    }
+})
 
